78378170dae050878422530e49d081cf1544c816
mehmet salih bindak <mehmetsalihbindak@gmail.com>
2018-08-10 15:39:06 +0300

    getting worse

--- src/App.js
+++ src/App.js
@@ -3,47 +3,75 @@ import {Platform, StyleSheet, Text, View,ScrollView,TextInput,AsyncStorage} from
 
 import {MyButton,ToDo} from './components'
 
-
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
   //var items = '{"Notes":[{"ID":"1","note":"selam"},{"ID":"2","note":"aleykim"}]}';
-  var Jnotes;
-  var notes = AsyncStorage.getItem("notes");
-  if (notes.length > 12){
-    Jnotes= JSON.parse(notes);
-  }else{
-    AsyncStorage.setItem("notes",'{"Notes":[{"ID":"123","note":"selamunaleykim"}]}'); // This line will work in the first time
-    Jnotes = JSON.parse(notes);
-    // {'Notes':[{'ID':'1','note':'aleykim'}]}
-  }
 
 export default class App extends Component{
   constructor(props){
     super(props);
 
     this.addItem = this.addItem.bind(this);
+    this.iniitalFunc = this.iniitalFunc.bind(this);
+    this.seeItems = this.seeItems.bind(this);
+
   }
 
   state = {
     toDO : '',
+    notes:'',
   }
 
-  addItem(){
-    if(this.state.toDO){
+  iniitalFunc(){
+    var initialValue = '{"Notes":[{"ID":"1","note":"selam"},{"ID":"2","note":"aleykim"}]}';
+    AsyncStorage.setItem("notes",initialValue);
+  }
+
+  seeItems(){
+    var notes = AsyncStorage.getItem("notes");
+
+    AsyncStorage.getItem("notes").then((value) => {
+      this.setState({"notes": value});
+  });
+
+    //Jnotes = JSON.parse(notes);
+    //Xnotes = notes.toString();
+    //alert(Jnotes.Notes[0].ID);
+    //alert(Xnotes);
+  }
+
+
+  addItem(){  
+  
       var date = Date.now().toString();
 
       // When app starts we have taken Notes and convert it Jnotes
       // Now we will take our new todo item and add to our Jnotes after J it.
+      //var notes = AsyncStorage.getItem("notes");
+      AsyncStorage.getItem("notes").then((value) => {
+        this.setState({"savingNote": value});
+    });
+     var saving;
+    sleep(500).then(() => {
+      saving = (this.state.savingNote);
+    
+    });
+    this.setState.aq = saving;
+    //asvar Jnotes = JSON.parse(saving);
+    
       
-          myObj = this.state.toDO;
-          JmyObj = JSON.parse(myObj);
-          Jnotes['Notes'].unshift({"ID":{date},"note": this.state.toDO});
-          sendNote = JSON.stringify(Jnotes);
-          AsyncStorage.setItem("notes",sendNote);
-          // Unshift means we add new item top of the list
+      //myObj = this.state.toDO;
+  
+      //Jnotes['Notes'].unshift({"ID":date,"note": myObj});
+      //sendNote = JSON.stringify(Jnotes);
+      //AsyncStorage.setItem("notes",sendNote);
+      // Unshift means we add new item top of the list
 
       // Clear the text area
-      this.setState({toDO:''})
-    }
-  }
+      this.setState({toDO:''});
+   
+}
 
   render() {
     return (
@@ -51,9 +79,7 @@ export default class App extends Component{
         <View style={styles.topOf}>
           <View style={styles.left}>
             <TextInput value={this.state.toDO} onChangeText={(v) => this.setState({toDO:v}) } placeholder={'Yapılacaklarım'} style={styles.input}>
-            <Text>
-             SELAMIN ALEYKİM
-            </Text>
+           
             
             </TextInput>
           </View>
@@ -62,16 +88,25 @@ export default class App extends Component{
           </View>
         </View>
         <View style={{backgroundColor:'black',height:0.5,marginHorizontal:10}}></View>
-     
-        
+     <View style={{height:300}}>
+
+           <MyButton onPress={this.iniitalFunc} text={'INITIAL'} />
+            <MyButton onPress={this.seeItems} text={'SEE'} />
+            <Text style={{flex:1}}>
+              {this.state.notes}
+            </Text>
+            <Text style={{flex:1}}>
+              {this.state.aq}
+            </Text>
+
+
+     </View>
         <ScrollView>
           
             {
               //items.map(((item) => <ToDo item={item} /> ))
             }
-         
-            
-             <ToDo item={Jnotes.Notes[0].note} />
+
           
 
         </ScrollView>
